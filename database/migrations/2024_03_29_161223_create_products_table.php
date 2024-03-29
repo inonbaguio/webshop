46aa9521b26bce14d7a254d2ca600bb51f530f44
Inon Baguio <inonbaguio@gmail.com>
2024-03-29 12:22:14 -0400

    Created initial model migrations

--- database/migrations/2024_03_29_161223_create_products_table.php
+++ database/migrations/2024_03_29_161223_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('main_image_id')->nullable();
             $table->timestamps();
         });
     }
