c57245b885226608592bda946b41c0b3da993088
Inon Baguio <inonbaguio@inons-mbp.home>
2024-03-29 12:21:34 -0400

    Created initial model migrations

--- database/migrations/2024_03_29_161820_create_images_table.php
+++ database/migrations/2024_03_29_161820_create_images_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('carts', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id')->nullable();
-            $table->string('session_id')->nullable();
+            $table->unsignedInteger('product_id');
+            $table->string('path');
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('images');
     }
 };
