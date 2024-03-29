2b60e8d8a0b9a96150d0fa1f4088e62495524e09
Inon Baguio <inonbaguio@gmail.com>
2024-03-29 13:54:44 -0400

    finished product page

---
+++ app/Livewire/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Product extends Component
+{
+    public $productId;
+
+    public function mount($productId)
+    {
+
+    }
+
+    public function getProductProperty()
+    {
+        return \App\Models\Product::findOrFail($this->productId);
+    }
+
+
+    public function render()
+    {
+        return view('livewire.product');
+    }
+}
